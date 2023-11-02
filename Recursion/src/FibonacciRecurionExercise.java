d32c4c2b7fd7a80329e2954e6348e82d137c2b54
kristinacortsen <eaa23krco@students.eaaa.dk>
2023-11-02 19:24:36 +0100

    Updated classes.

---
+++ Recursion/src/FibonacciRecurionExercise.java
@@ -0,0 +1,28 @@
+public class FibonacciRecurionExercise {
+
+    private static long[] storedNumbers;
+
+
+    public static void main(String[] args) {
+
+        int n = 92;   // max for long
+        storedNumbers = new long[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            System.out.println(fibonacci(i));
+        }
+    }
+
+    private static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        if (storedNumbers[n] != 0) {
+            return storedNumbers[n];
+        }
+        long fibonacciNumbers = fibonacci(n - 1) + fibonacci(n - 2);
+        storedNumbers[n] = fibonacciNumbers;
+
+        return fibonacciNumbers;
+    }
+}
